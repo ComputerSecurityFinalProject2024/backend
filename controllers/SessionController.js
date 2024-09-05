1b2e4cf24e02e3c9424b3e638c3b1f58e1618ae5
Zyken <leviettt.tnh@gmail.com>
2024-09-05 17:50:21 +0700

    upd: Add Login & Authorize to SessionController

--- controllers/SessionController.js
+++ controllers/SessionController.js
@@ -1,5 +1,9 @@
 const createError = require("http-errors");
 const prisma = require("../prisma/prisma");
+const { authenticationServer } = require("../lib/AuthenticationServer");
+const { ticketGrantingServer } = require("../lib/TicketGrantingServer");
+const { hasher } = require("../lib/HashHandler");
+require("dotenv").config();
 
 class SessionController {
 	// [POST] /sessions
@@ -39,13 +43,38 @@ class SessionController {
 
 	// [POST] /sessions/:sessionId/users/logIn
 	async logIn(req, res, next) {
-		res.status(200).json({ status: 200, message: "User logged in" });
+		const { username, password } = req.body;
+		try {
+			let asMessage = await authenticationServer.authenticateUser(username, password);
+			let tgs_session_key = hasher.decrypt(asMessage.tgs_session_key, process.env.CLIENT_SECRET_KEY);
+			let tgt_obj = hasher.decrypt(asMessage.tgt, tgs_session_key);
+			if (tgt_obj.split("//")[0] !== username) {
+				throw createError(401, "Invalid username");
+			}
+			process.env.TGS_SESSION_KEY = tgs_session_key;
+			process.env.USERNAME = username;
+			process.env.TGT = asMessage.tgt;
+			res.status(200).json({ status: 200, message: "User logged in" });
+		} catch (err) {
+			next(err);
+		}
 	}
 
 	// [POST] /sessions/:sessionId/services/:serviceId:authorize
 	async authorize(req, res, next) {
 		console.log(req.params);
-		res.status(200).json({ status: 200, message: "Service authorized" });
+		const serviceId = req.params.serviceId;
+		try {
+			let newReq = {
+				tgt: process.env.TGT,
+				service_id: serviceId,
+			};
+			let authenticator = hasher.encrypt(process.env.USERNAME + "//" + (Date.now() + 1000 * 60 * 60).toString(), process.env.TGS_SESSION_KEY);
+			let tgsMessage = await ticketGrantingServer.validateTGT(newReq, authenticator);
+			res.status(200).json({ status: 200, message: "Service authorized" });
+		} catch (err) {
+			next(err);
+		}
 	}
 
 	// [POST] /sessions/:sessionId/services/:serviceId:handshake
--- test.js
+++ test.js
@@ -1,13 +1,26 @@
-const RSA = require('./lib/rsa');
+const as = require('./lib/AuthenticationServer.js');
+const tgs = require('./lib/TicketGrantingServer.js');
+const ss = require('./lib/ServiceServer.js');
+const hasher = require('./lib/HashHandler.js');
 
-const rsa = new RSA();
+let username = 'viet';
+let as_message = as.authenticateUser(username);
+console.log('as_message: ', as_message);
 
-const message = "Hello, RSA";
+let tgs_session_key = hasher.decrypt(as_message.tgs_session_key, process.env.CLIENT_SECRET_KEY);
+let authenticator = hasher.encrypt(username + "//" + (Date.now() + 1000 * 60 * 60).toString(), tgs_session_key);
 
-// Mã hóa thông điệp
-const encryptedMessage = rsa.encrypt(message);
-console.log("Encrypted Message:", encryptedMessage);
+let req_message = {
+    tgt: as_message.tgt,
+    service_id: 'service_id',
+}
 
-// Giải mã thông điệp
-const decryptedMessage = rsa.decrypt(encryptedMessage);
-console.log("Decrypted Message:", decryptedMessage);
+let tgs_message = tgs.validateTGT(req_message, authenticator);
+console.log('tgs_message: ', tgs_message);
+
+let client_to_server_ticket = tgs_message.client_to_server_ticket;
+
+authenticator = hasher.encrypt(username + "//" + (Date.now() + 1000 * 60 * 60).toString(), tgs_message.client_server_session_key);
+
+let ss_message = ss.validateClientToServerTicket(client_to_server_ticket, authenticator);
+console.log('ss_message: ', ss_message);
