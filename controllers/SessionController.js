d7b282f74373ba440037edb3d62938398ff07080
Zyken <leviettt.tnh@gmail.com>
2024-09-06 07:29:18 +0700

    upd: add Authorize to Session Control

--- controllers/SessionController.js
+++ controllers/SessionController.js
@@ -1,9 +1,13 @@
 const createError = require("http-errors");
 const prisma = require("../prisma/prisma");
-const { authenticationServer } = require("../lib/AuthenticationServer");
-const { ticketGrantingServer } = require("../lib/TicketGrantingServer");
-const { hasher } = require("../lib/HashHandler");
+const authenticationServer = require("../lib/AuthenticationServer");
+const ticketGrantingServer = require("../lib/TicketGrantingServer");
+const hasher = require("../lib/HashHandler");
 require("dotenv").config();
+
+const TGS_session_key = 'wPWkODvBBJmxzqQg';
+const TGS_secret_key = 'AbcdefGhijklmnop';
+const Service_secret_key = 'QwertyUiopAsdfgh';
 
 class SessionController {
 	// [POST] /sessions
@@ -43,18 +47,20 @@ class SessionController {
 
 	// [POST] /sessions/:sessionId/users/logIn
 	async logIn(req, res, next) {
-		const { username, password } = req.body;
+		const { username } = req.body;
 		try {
-			let asMessage = await authenticationServer.authenticateUser(username, password);
-			let tgs_session_key = hasher.decrypt(asMessage.tgs_session_key, process.env.CLIENT_SECRET_KEY);
-			let tgt_obj = hasher.decrypt(asMessage.tgt, tgs_session_key);
-			if (tgt_obj.split("//")[0] !== username) {
-				throw createError(401, "Invalid username");
+			console.log(hasher);
+			let asMessage = await authenticationServer.authenticateUser(username);
+			console.log('asMessage', asMessage);
+			if (asMessage == null) {
+				next(createError(401, "Invalid username"));
 			}
-			process.env.TGS_SESSION_KEY = tgs_session_key;
-			process.env.USERNAME = username;
-			process.env.TGT = asMessage.tgt;
-			res.status(200).json({ status: 200, message: "User logged in" });
+			// let tgs_session_key = hasher.decrypt(asMessage.tgs_session_key, process.env.CLIENT_SECRET_KEY);
+			// let tgt_obj = hasher.decrypt(asMessage.tgt, tgs_session_key);
+
+			as_Message = asMessage;
+			
+			res.status(200).json({ status: 200, message: asMessage });
 		} catch (err) {
 			next(err);
 		}
@@ -63,15 +69,19 @@ class SessionController {
 	// [POST] /sessions/:sessionId/services/:serviceId:authorize
 	async authorize(req, res, next) {
 		console.log(req.params);
-		const serviceId = req.params.serviceId;
+		const { message, authenticator } = req.body;
 		try {
-			let newReq = {
-				tgt: process.env.TGT,
-				service_id: serviceId,
-			};
-			let authenticator = hasher.encrypt(process.env.USERNAME + "//" + (Date.now() + 1000 * 60 * 60).toString(), process.env.TGS_SESSION_KEY);
-			let tgsMessage = await ticketGrantingServer.validateTGT(newReq, authenticator);
-			res.status(200).json({ status: 200, message: "Service authorized" });
+			console.log('message:', message);
+			console.log('authenticator:', authenticator);
+			const tgs_message = await ticketGrantingServer.validateTGT(message, authenticator);
+			
+			if (tgs_message == null) {
+				next(createError(401, "TGT lifetime expired"));
+			}
+
+			console.log('tgs_message:', tgs_message);
+
+			res.status(200).json({ status: 200, message: tgs_message });
 		} catch (err) {
 			next(err);
 		}
--- test.js
+++ test.js
@@ -3,24 +3,9 @@ const tgs = require('./lib/TicketGrantingServer.js');
 const ss = require('./lib/ServiceServer.js');
 const hasher = require('./lib/HashHandler.js');
 
-let username = 'viet';
-let as_message = as.authenticateUser(username);
-console.log('as_message: ', as_message);
-
-let tgs_session_key = hasher.decrypt(as_message.tgs_session_key, process.env.CLIENT_SECRET_KEY);
-let authenticator = hasher.encrypt(username + "//" + (Date.now() + 1000 * 60 * 60).toString(), tgs_session_key);
-
-let req_message = {
-    tgt: as_message.tgt,
-    service_id: 'service_id',
-}
-
-let tgs_message = tgs.validateTGT(req_message, authenticator);
-console.log('tgs_message: ', tgs_message);
-
-let client_to_server_ticket = tgs_message.client_to_server_ticket;
-
-authenticator = hasher.encrypt(username + "//" + (Date.now() + 1000 * 60 * 60).toString(), tgs_message.client_server_session_key);
-
-let ss_message = ss.validateClientToServerTicket(client_to_server_ticket, authenticator);
-console.log('ss_message: ', ss_message);
+const tgt = 'viet//wPWkODvBBJmxzqQg//' + (Date.now() + 1000 * 60 * 60).toString();
+const encryptedTgt = hasher.encrypt(tgt, 'AbcdefGhijklmnop');
+const authenticator = hasher.encrypt('viet//' + (Date.now() + 1000 * 60 * 60).toString(), 'wPWkODvBBJmxzqQg');
+console.log(tgt);   
+console.log(encryptedTgt);
+console.log(authenticator);
