5fddb5d29e21ce095236120a53b32bb4f5666030
Zyken <leviettt.tnh@gmail.com>
2024-09-07 20:42:54 +0700

    upd: add Handshake to SessionController

--- test.js
+++ test.js
@@ -3,9 +3,16 @@ const tgs = require('./lib/TicketGrantingServer.js');
 const ss = require('./lib/ServiceServer.js');
 const hasher = require('./lib/HashHandler.js');
 
-const tgt = 'viet//wPWkODvBBJmxzqQg//' + (Date.now() + 1000 * 60 * 60).toString();
-const encryptedTgt = hasher.encrypt(tgt, 'AbcdefGhijklmnop');
-const authenticator = hasher.encrypt('viet//' + (Date.now() + 1000 * 60 * 60).toString(), 'wPWkODvBBJmxzqQg');
-console.log(tgt);   
-console.log(encryptedTgt);
-console.log(authenticator);
+const TGS_secret_key = hasher.generateKey();
+const Service_secret_key = hasher.generateKey();
+const TGS_session_key = hasher.generateKey() + "//" + (Date.now() + 1000 * 60 * 60 * 24 * 7).toString();
+
+const tgt = hasher.encrypt("viet//" + TGS_session_key, TGS_secret_key);
+const authenticator = hasher.encrypt("viet//" + (Date.now() + 1000 * 60 * 60 * 24 * 7).toString(), "qLlCfXLRXwvlwefr");
+
+console.log('tgt:', tgt);
+console.log('authenticator:', authenticator);
+console.log('TGS_session_key:', TGS_session_key);
+console.log('TGS_secret_key:', TGS_secret_key);
+console.log('Service_secret_key:', Service_secret_key);
+
